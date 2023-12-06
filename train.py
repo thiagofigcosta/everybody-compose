b4af244746502cfb15d7d59e947d1445b2ebac8b
Aian Shay <39931013+aianshay@users.noreply.github.com>
2023-12-06 12:23:51 -0300

    Add genre argument to training

--- train.py
+++ train.py
@@ -1,13 +1,14 @@
 import argparse
 from utils.model import train
-import torch
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('Train DeepBeats')
     parser.add_argument('-m','--model_name', type=str)
+    parser.add_argument('-g', '--genre', type=str, default='classical')
     parser.add_argument('-nf','--n_files', type=int, default=-1)
     parser.add_argument('-n','--n_epochs', type=int, default=100)
-    parser.add_argument('-d','--device', type=str, default='cuda' if torch.cuda.is_available() else 'cpu')
+    parser.add_argument('-d','--device', type=str,  default='cuda' if torch.cuda.is_available() else 'cpu')
     parser.add_argument('-s','--snapshots_freq', type=int, default=200)
     parser.add_argument('-c','--checkpoint', type=str, default=None)
     args = parser.parse_args()
